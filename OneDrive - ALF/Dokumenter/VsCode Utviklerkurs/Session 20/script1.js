c076579704abd9cf26e6031405c722b97640ddc8
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 13:28:15 +0200

    Update script1.js

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script1.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script1.js
@@ -7,7 +7,7 @@
 
 
 
-                document.getElementById("output1").innerHTML = number + 2;
+                document.getElementById("output1").innerHTML = number / 2;
                 document.getElementById("output2").innerHTML = number * 2;
                 document.getElementById("output3").innerHTML = number * 3;
                 document.getElementById("output4").innerHTML = number * number;
