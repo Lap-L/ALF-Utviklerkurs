0ed12cb357d8200a0672b78cfcb1344540a2c922
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 12:34:35 +0200

    Add email generator HTML and JS functionality
    Created Oppgave6.html with input fields for first and last name, and a button to generate an email address. Added script6.js to handle input retrieval and email formatting, displaying the result in the output element.

---
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script6.js
@@ -0,0 +1,17 @@
+          
+          
+
+        function hentVerdier1() {
+
+            let name1 = document.getElementById("name1").value;
+            let name2 = document.getElementById("name2").value;
+            let a, b;
+            a = name1 + "." + name2
+            b = a + "@olsenbanden.co.uk"
+
+
+
+            document.getElementById("output1").innerHTML = b
+            
+}
+  
