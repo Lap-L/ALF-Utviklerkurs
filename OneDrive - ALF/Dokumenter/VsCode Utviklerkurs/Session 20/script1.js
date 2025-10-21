e0730542a2d63c9316363b648a9cef5fc9169664
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 13:27:48 +0200

    Update script1.js

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script1.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script1.js
@@ -2,12 +2,12 @@
           
           function hentVerdier() {
 
-                let number = document.getElementById("number").value;
+                let number = Number(document.getElementById("number").value);
                 
 
 
 
-                document.getElementById("output1").innerHTML = number;
+                document.getElementById("output1").innerHTML = number + 2;
                 document.getElementById("output2").innerHTML = number * 2;
                 document.getElementById("output3").innerHTML = number * 3;
                 document.getElementById("output4").innerHTML = number * number;
