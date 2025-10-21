2b76d7c269829f47d6724a55a3d12b206ce96abf
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 11:59:14 +0200

    Add Oppgave5 HTML and JS for text manipulation
    Created Oppgave5.html with input field and buttons to get text length, convert to uppercase, and lowercase. Added script5.js implementing the required functions to update the output fields based on user input. Also updated titles in Oppgave1-4.html to specify the respective task.

---
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script5.js
@@ -0,0 +1,39 @@
+          
+          
+
+        function hentVerdier1() {
+
+            let text = document.getElementById("text").value;
+            
+            let len = text.length;
+
+
+
+            document.getElementById("output1").innerHTML = len
+            
+}
+  
+function hentVerdier2() {
+
+            let text = document.getElementById("text").value;
+            
+            let up = text.toUpperCase();
+
+
+
+            document.getElementById("output2").innerHTML = up
+            
+}
+  
+function hentVerdier3() {
+
+            let text = document.getElementById("text").value;
+            
+            let low = text.toLowerCase();
+
+
+
+            document.getElementById("output3").innerHTML = low
+            
+}
+  
