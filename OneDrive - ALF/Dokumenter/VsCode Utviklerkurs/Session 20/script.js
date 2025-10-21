5f5e9220dc9041c951c449914706c51a1938bf77
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 09:57:20 +0200

    Add HTML, JS, and CSS files for input demo
    Introduces Oppgave1.html with various input elements and a button to fetch values, script.js to handle value retrieval and display, and renames style1.css for styling. This sets up a basic interactive form for demonstration purposes.

---
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script.js
@@ -0,0 +1,17 @@
+          
+          
+          function hentVerdier() {
+
+                let text = document.getElementById("text").value;
+                let area = document.getElementById("area").value;
+                let valg = document.getElementById("valg").value;
+                let color = document.getElementById("color").value;
+                let date = document.getElementById("date").value;
+                let lorem = document.getElementById("lorem").value;
+
+
+
+                document.getElementById("output").innerHTML = text + " " + area + " " + valg + " " + color + " " + date;
+
+}
+  
