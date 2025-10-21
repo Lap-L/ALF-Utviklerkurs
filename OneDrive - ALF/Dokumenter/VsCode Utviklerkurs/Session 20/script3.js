ab3e39e98c7943eb67c415fb5b29535f047acbd6
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 11:21:46 +0200

    Update conversion from temperature to speed units
    Changed Oppgave3.html and script3.js to convert between km/h and mp/h instead of temperature units. Added Oppgave4.html and script4.js for similar speed conversion functionality. Updated input placeholders, output labels, and calculation logic accordingly.

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script3.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script3.js
@@ -4,25 +4,18 @@
         function hentVerdier() {
 
             let number1 = document.getElementById("number1").value;
-            let a, b, c;
-            a = number1 - 32
-            b = a / 1.8
-            c = b + 273
+            let a ;
+            a = number1 * 1.60934
             
             let number2 = document.getElementById("number2").value;
-            let d, e, f, g;
-            d = number2 *1.8
-            e = d + 32
-            f = d / 1.8
-            g = f + 273
+            let d ;
+            d = number2 / 1.60934
 
 
 
-            document.getElementById("output1").innerHTML = b;
-            document.getElementById("output2").innerHTML = c ;
-            document.getElementById("output3").innerHTML = e;
-            document.getElementById("output4").innerHTML = g;
-                
+            document.getElementById("output1").innerHTML = a;
+            document.getElementById("output2").innerHTML = d ;
+            
 
 }
   
---
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script4.js
@@ -0,0 +1,21 @@
+          
+          
+
+        function hentVerdier() {
+
+            let number1 = document.getElementById("number1").value;
+            let a ;
+            a = number1 * 1.60934
+            
+            let number2 = document.getElementById("number2").value;
+            let d ;
+            d = number2 / 1.60934
+
+
+
+            document.getElementById("output1").innerHTML = a;
+            document.getElementById("output2").innerHTML = d ;
+            
+
+}
+  
