22110d9815875e4b58c4578243ea0f25bfaacf8d
Lap-L <jacobnigeljensen@gmail.com>
2025-10-21 10:32:28 +0200

    Refactor Oppgave1 and add files for new tasks
    Updated Oppgave1.html to use a number input and display calculated values, replacing previous text and select inputs. Replaced script.js with script1.js to match new functionality. Added placeholder HTML and JS files for Oppgave2-6 and included favicon.ico.

---
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 20/script1.js
@@ -0,0 +1,16 @@
+          
+          
+          function hentVerdier() {
+
+                let number = document.getElementById("number").value;
+                
+
+
+
+                document.getElementById("output1").innerHTML = number;
+                document.getElementById("output2").innerHTML = number * 2;
+                document.getElementById("output3").innerHTML = number * 3;
+                document.getElementById("output4").innerHTML = number * number;
+
+}
+  
