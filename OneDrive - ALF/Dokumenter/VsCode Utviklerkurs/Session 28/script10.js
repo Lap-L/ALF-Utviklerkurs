f282a7f178c31f6fa8a7b4b72a9532a290f9257f
Lap-L <jacobnigeljensen@gmail.com>
2025-11-07 13:06:30 +0100

    Fix input handling and increment logic in loop
    Changed input retrieval from innerHTML to value for number fields and updated the loop to increment by 1 instead of 5. Also removed unnecessary semicolon from button onclick attribute.

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 28/script10.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 28/script10.js
@@ -7,13 +7,13 @@
             let tall;
             let innhold = "";
 
-            let lim1 = Number(document.getElementById("number1").innerHTML);
-            let lim2 = Number(document.getElementById("number2").innerHTML);
+            let lim1 = Number(document.getElementById("number1").value);
+            let lim2 = Number(document.getElementById("number2").value);
                    
                     tall = lim1;
                     while (tall <= lim2) {
                         innhold += "Jeg er tall nummer " + tall + "<br>";
-                        tall += 5;
+                        tall ++;
                     }
 
             document.getElementById("output1").innerHTML= innhold;
