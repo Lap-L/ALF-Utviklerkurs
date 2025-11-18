981df6cce1e9928c1aa2722ebdaa5ad9206c271c
Lap-L <jacobnigeljensen@gmail.com>
2025-11-18 11:56:20 +0100

    m

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script3.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script3.js
@@ -1,4 +1,132 @@
-// Session 31 - script3 placeholder
-function hentVerdier() {
-    // placeholder
-}
+
+        
+        
+        function array() {
+       
+         
+         let Brann = [
+		{ fornavn: "Donald",
+		  etternavn: "Duck",
+          draktnummer: "1",
+		  posisjon: "Keeper",
+          
+		 },
+
+		{ fornavn: "Ole",
+		  etternavn: "Duck",
+          draktnummer: "2",
+		  posisjon: "Forsvarspiller",
+		 }
+         ,
+
+		{ fornavn: "Dole",
+		  etternavn: "Duck",
+          draktnummer: "3",
+		  posisjon: "Midtbanespiller",
+		 }
+         ,
+
+		{ fornavn: "Doffen",
+		  etternavn: "Duck",
+          draktnummer: "4",
+		  posisjon: "Angrepspiller",
+		 },
+
+         { fornavn: "Donald2",
+		  etternavn: "Duck2",
+          draktnummer: "5",
+		  posisjon: "Keeper",
+          
+		 },
+
+		{ fornavn: "Ole2",
+		  etternavn: "Duck2",
+          draktnummer: "6",
+		  posisjon: "Forsvarspiller",
+		 }
+         ,
+
+		{ fornavn: "Dole2",
+		  etternavn: "Duck2",
+          draktnummer: "7",
+		  posisjon: "Midtbanespiller",
+		 }
+         ,
+
+		{ fornavn: "Doffen2",
+		  etternavn: "Duck2",
+          draktnummer: "8",
+		  posisjon: "Angrepspiller",
+		 },
+
+         { fornavn: "Donald3",
+		  etternavn: "Duck3",
+          draktnummer: "9",
+		  posisjon: "Keeper",
+          
+		 },
+
+		{ fornavn: "Ole3",
+		  etternavn: "Duck3",
+          draktnummer: "10",
+		  posisjon: "Forsvarspiller",
+		 }
+         ,
+
+		{ fornavn: "Dole3",
+		  etternavn: "Duck3",
+          draktnummer: "11",
+		  posisjon: "Midtbanespiller"
+		 }
+         
+         
+		]	
+
+        svar = ""
+        svar2 = ""
+        svar3 = ""
+
+        for(let i = 0; i < Brann.length; i++) {
+
+            console.log(Brann[i].fornavn)
+
+            svar += "<br>" + Brann[i].fornavn + " " + Brann[i].etternavn + " har draktnummer " + Brann[i].draktnummer + ", og er " + Brann[i].posisjon + ". <br>" 
+
+            if (Brann[i].posisjon === "Angrepspiller") {
+                 svar2 += "<br>" + "angrepspiller : " + Brann[i].fornavn + " " + Brann[i].etternavn + ". <br>" ;
+        }
+             
+        if ( ops === "Angrepspiller" ) {
+                Brann[i].posisjon === "Angrepspiller"
+                svar3 += "<br>" + Brann[i].fornavn + " " + Brann[i].etternavn + ". <br>" ;
+               
+            } else if ( ops === "Keeper" ) {
+                Brann[i].posisjon === "Keeper"
+                svar3 += "<br>" + Brann[i].fornavn + " " + Brann[i].etternavn + ". <br>" ;          
+
+            }
+            else if ( ops === "Forsvarspiller" ) {
+                Brann[i].posisjon === "Forsvarspiller"
+                svar3 += "<br>" + Brann[i].fornavn + " " + Brann[i].etternavn + ". <br>" ;      
+
+            }
+             else {
+                Brann[i].posisjon === "Midtbanespiller"
+                svar3 += "<br>" + Brann[i].fornavn + " " + Brann[i].etternavn + ". <br>" ;
+
+            }
+
+        }
+
+         document.getElementById("output1").innerHTML += svar;
+         document.getElementById("output2").innerHTML += svar2;
+         document.getElementById("output3").innerHTML += svar3;
+         
+         
+
+
+            
+        }
+
+        
+
--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script4.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script4.js
@@ -1,4 +1,41 @@
-// Session 31 - script4 placeholder
-function hentVerdier() {
-    // placeholder
+          
+
+        function calc() {
+
+            let number1 = Number(document.getElementById("number1").value);
+            let number2 = Number(document.getElementById("number2").value);
+            let ops = document.getElementById("ops").value;
+            let calc1, calc2, calc3, calc4;
+            
+            calc1 = Number(number1 + number2) ;
+            calc2 = number1 - number2 ;
+            calc3 = number1 * number2 ;
+            calc4 = number1 / number2 ;
+                 
+            
+                document.getElementById("output1").innerHTML = number1;
+                document.getElementById("output2").innerHTML = ops;
+                document.getElementById("output3").innerHTML = number2;
+
+        if ( ops === "+" ) {
+                document.getElementById("output4").innerHTML = Number(calc1);
+               
+            } else if ( ops === "-" ) {
+                document.getElementById("output4").innerHTML = calc2;           
+
+            }
+            else if ( ops === "*" ) {
+                document.getElementById("output4").innerHTML = calc3;           
+
+            }
+             else {
+                document.getElementById("output4").innerHTML = calc4;
+
+            }
+                
+                
+            
 }
+  
+
+                
