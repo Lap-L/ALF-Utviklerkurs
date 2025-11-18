1c774bd0308360726e7aa7627e4de43a6b06d9e4
Lap-L <jacobnigeljensen@gmail.com>
2025-11-18 10:25:13 +0100

    a

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 30/script7.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 30/script7.js
@@ -6,25 +6,16 @@
          
          let arr = ["Hans","Ole","Nils","Olav","Per","Knut","Kari","Line","Pia"];
          let ny = arr[Math.floor (Math.random() * 9 )];
-         arr2 += []
-        const Hans = "Hans";
-
-if (arr2.indexOf(Hans) !== -1) {
-  document.getElementById("output1").innerHTML= "";
-} else {
-  document.getElementById("output1").innerHTML= ;
-}
-            
-    
-
-            document.getElementById("output1").innerHTML +=ny;
-        }
-
+         
          
 
+        
+
+         document.getElementById("output1").innerHTML += ny + ", ";
+
 
             
-        
+        }
 
         
 
--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script1.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script1.js
@@ -1,4 +1,63 @@
-// Session 31 - script1 placeholder
-function hentVerdier() {
-    // placeholder
-}
+
+        
+        
+        function array() {
+       
+         
+         let mineDyr = [
+		{ navn: "Katt1",
+		  fodselsAar: 2020,
+		  mor: "Kattmor1",
+          far: "Kattfar1",
+          rase: "skog1"
+		  
+		 },
+
+		{ navn: "Katt2",
+		  fodselsAar: 2010,
+		  mor: "Kattmor2",
+          far: "Kattfar2",
+          rase: "skog2"
+		 }
+         ,
+
+		{ navn: "Katt3",
+		  fodselsAar: 2015,
+		  mor: "Kattmor3",
+          far: "Kattfar3",
+          rase: "skog3"
+		 }
+         ,
+
+		{ navn: "Katt4",
+		  fodselsAar: 2018,
+		  mor: "Kattmor4",
+          far: "Kattfar4",
+          rase: "skog4"
+		 }
+         ,
+
+		{ navn: "Katt5",
+		  fodselsAar: 2009,
+		  mor: "Kattmor5",
+          far: "Kattfar5",
+          rase: "skog5"
+		 }
+		]	
+
+        for(let i = 0; i < mineDyr.length; i++) {
+
+            console.log(mineDyr[i].navn)
+
+            svar +=  `${mineDyr[i].navn} ble født ${mineDyr[i].fodselsAar} <br>`
+        }
+
+
+         document.getElementById("output1").innerHTML += ny + ", ";
+
+
+            
+        }
+
+        
+
