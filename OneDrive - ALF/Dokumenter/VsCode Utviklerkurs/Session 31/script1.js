99a9d84c37c7340eeca625f5e107c1e976dcf270
Lap-L <jacobnigeljensen@gmail.com>
2025-11-18 11:13:23 +0100

    a

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script1.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script1.js
@@ -7,6 +7,7 @@
          let mineDyr = [
 		{ navn: "Katt1",
 		  fodselsAar: 2020,
+          alder: 5,
 		  mor: "Kattmor1",
           far: "Kattfar1",
           rase: "skog1"
@@ -15,6 +16,7 @@
 
 		{ navn: "Katt2",
 		  fodselsAar: 2010,
+          alder: 15,
 		  mor: "Kattmor2",
           far: "Kattfar2",
           rase: "skog2"
@@ -23,6 +25,7 @@
 
 		{ navn: "Katt3",
 		  fodselsAar: 2015,
+          alder: 10,
 		  mor: "Kattmor3",
           far: "Kattfar3",
           rase: "skog3"
@@ -31,6 +34,7 @@
 
 		{ navn: "Katt4",
 		  fodselsAar: 2018,
+          alder: 7,
 		  mor: "Kattmor4",
           far: "Kattfar4",
           rase: "skog4"
@@ -39,21 +43,31 @@
 
 		{ navn: "Katt5",
 		  fodselsAar: 2009,
+          alder: 16,
 		  mor: "Kattmor5",
           far: "Kattfar5",
           rase: "skog5"
 		 }
 		]	
 
+        svar = ""
+        svar2 = ""
+        svar3 = ""
+
         for(let i = 0; i < mineDyr.length; i++) {
 
             console.log(mineDyr[i].navn)
 
-            svar +=  `${mineDyr[i].navn} ble født ${mineDyr[i].fodselsAar} <br>`
+            svar += "<br>" + mineDyr[i].navn + " ble født " + mineDyr[i].fodselsAar + "<br>"
+            svar2 += "<br>" +  mineDyr[i].navn + " sin mor heter "  + mineDyr[i].mor + ", og faren heter " +  mineDyr[i].far + "<br>"
+            svar3 += "<br>" +  mineDyr[i].navn + " er nå "  + mineDyr[i].alder + " år " + "<br>"
+            
         }
 
 
-         document.getElementById("output1").innerHTML += ny + ", ";
+         document.getElementById("output1").innerHTML += svar;
+         document.getElementById("output2").innerHTML += svar2;
+         document.getElementById("output3").innerHTML += svar3;
 
 
             
--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script2.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 31/script2.js
@@ -1,4 +1,102 @@
-// Session 31 - script2 placeholder
-function hentVerdier() {
-    // placeholder
-}
+
+        
+        
+        function array() {
+       
+         
+         let Duck = [
+		{ fornavn: "Donald",
+		  etternavn: "Duck",
+          adresse: "andeby",
+		  poststed: "1000",
+          telefon: { 
+            privat: 123 ,
+            jobb: 456
+			},
+            epost: { 
+                privat: "donald@duck.no",
+                jobb: "donald@andeby.no"
+
+			},
+		 },
+
+		{ fornavn: "Ole",
+		  etternavn: "Duck",
+          adresse: "andeby",
+		  poststed: "1000",
+          telefon: { 
+            privat: 1234 ,
+            jobb: 4567
+			},
+            epost: { 
+                privat: "Ole@duck.no",
+                jobb: "Ole@andeby.no"
+
+			},
+		 }
+         ,
+
+		{ fornavn: "Dole",
+		  etternavn: "Duck",
+          adresse: "andeby",
+		  poststed: "1000",
+          telefon: { 
+            privat: 12345 ,
+            jobb: 45678
+			},
+            epost: { 
+                privat: "Dole@duck.no",
+                jobb: "Dole@andeby.no"
+
+			},
+		 }
+         ,
+
+		{ fornavn: "Doffen",
+		  etternavn: "Duck",
+          adresse: "andeby",
+		  poststed: "1000",
+          telefon: { 
+            privat: 123456 ,
+            jobb: 456789
+			},
+            epost: { 
+                privat: "Doffen@duck.no",
+                jobb: "Doffen@andeby.no"
+
+			},
+		 }
+         
+		]	
+
+        svar = ""
+        svar2 = ""
+        svar3 = ""
+
+        for(let i = 0; i < Duck.length; i++) {
+
+            console.log(Duck[i].navn)
+
+            svar += "<br>" + "fornavn: " + Duck[i].fornavn + "<br>" + "etternavn: "+ Duck[i].etternavn + "<br>"
+            + "adresse: " + Duck[i].adresse + "<br>" + "poststed: "+ Duck[i].poststed + "<br>"
+            + "telefon: " + "<br>" + "privat: " + Duck[i].telefon.privat + "<br>" + "jobb: " + Duck[i].telefon.jobb + "<br>"
+            + "epost: " + "<br>" + "privat: " + Duck[i].epost.privat + "<br>" + "jobb: " + Duck[i].epost.jobb + "<br>"
+            svar2 += "<br>" + "Epost-adressen til " + Duck[i].fornavn + " er " + Duck[i].epost.privat + "<br>"
+            svar3 += "<br>" + "fornavn: " + Duck[i].fornavn + "<br>" + "etternavn: "+ Duck[i].etternavn + "<br>"
+            + "jobb tlf: " + Duck[i].telefon.jobb + "<br>"
+            
+            
+        }
+
+
+         document.getElementById("output1").innerHTML += svar;
+         document.getElementById("output2").innerHTML += svar2;
+         document.getElementById("output3").innerHTML += svar3;
+         
+
+
+            
+        }
+
+        
+
