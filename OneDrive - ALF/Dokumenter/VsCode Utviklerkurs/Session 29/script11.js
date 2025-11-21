a8b0759e1bf68fc05591497788cc100457f6bd31
Lap-L <jacobnigeljensen@gmail.com>
2025-11-21 11:24:49 +0100

    Update script11.js

--- OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 29/script11.js
+++ OneDrive - ALF/Dokumenter/VsCode Utviklerkurs/Session 29/script11.js
@@ -22,3 +22,18 @@
 }
 
         
+        function oppstart2 () {
+            let utskrift = document.getElementById("output");
+            resultat = "";
+            let teller = 10; // har kontroll på vor mange tall som skal skrives ut på hver linje
+
+            for (num=1; num <= 10; num++){ // flytter til neste linje 
+                for (i=1; i <= teller; i++){ // skriver ut hver enkelt linje
+                    resultat += i + " ";
+                }
+                resultat += "<br>";
+                teller--; // Begynner på 10 (første linje) og reduseres med 1 for hver linje       
+            }
+            utskrift.innerHTML = resultat;
+        }
+    
